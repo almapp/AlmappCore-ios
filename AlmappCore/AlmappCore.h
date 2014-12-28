274d9cc9ec016df3f1d9942f3119890ed2bbe6bd
Patricio López <lopezjuripatricio@gmail.com>
2014-12-28 10:53:18 -0300

    initial commit
    Following instructions from:
    http://blog.sigmapoint.pl/developing-static-library-for-ios-with-cocoapo
    ds/

---
+++ AlmappCore/AlmappCore.h
@@ -0,0 +1,13 @@
+//
+//  AlmappCore.h
+//  AlmappCore
+//
+//  Created by Patricio López on 28-12-14.
+//  Copyright (c) 2014 almapp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AlmappCore : NSObject
+
+@end
---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '7.0'
+
+target 'AlmappCore' do
+pod 'RestKit', '~> 0.24.0'
+pod 'Realm'
+end
+
+target 'AlmappCoreTests' do
+
+end
+
