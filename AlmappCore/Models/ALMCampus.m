429df593117172ac29cca4f0084b227c5f8eb15a
Patricio López <lopezjuripatricio@gmail.com>
2014-12-31 17:23:22 -0300

    added missing ALMCampus class

---
+++ AlmappCore/Models/ALMCampus.m
@@ -0,0 +1,17 @@
+//
+//  ALMCampus.m
+//  AlmappCore
+//
+//  Created by Patricio López on 31-12-14.
+//  Copyright (c) 2014 almapp. All rights reserved.
+//
+
+#import "ALMCampus.h"
+
+@implementation ALMCampus
+
++ (NSString *)pluralForm {
+    return @"campuses";
+}
+
+@end
