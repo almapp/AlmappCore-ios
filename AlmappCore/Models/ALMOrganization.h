36d6cdcf54b983aa74f9cf1bca32a82ff2afcb5b
Patricio López <lopezjuripatricio@gmail.com>
2015-01-02 12:33:54 -0300

    added Organization model

---
+++ AlmappCore/Models/ALMOrganization.h
@@ -0,0 +1,13 @@
+//
+//  ALMOrganization.h
+//  AlmappCore
+//
+//  Created by Patricio López on 02-01-15.
+//  Copyright (c) 2015 almapp. All rights reserved.
+//
+
+#import "ALMArea.h"
+
+@interface ALMOrganization : ALMArea
+
+@end
---
+++ AlmappCore/Models/ALMOrganization.m
@@ -0,0 +1,17 @@
+//
+//  ALMOrganization.m
+//  AlmappCore
+//
+//  Created by Patricio López on 02-01-15.
+//  Copyright (c) 2015 almapp. All rights reserved.
+//
+
+#import "ALMOrganization.h"
+
+@implementation ALMOrganization
+
+- (NSString *)areaClassType {
+    return @"ALMOrganization";
+}
+
+@end
