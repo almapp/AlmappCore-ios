4ce36f30e2830ed7c4537ab7b7be2b0bf59e0955
Patricio López <lopezjuripatricio@gmail.com>
2015-01-15 10:53:45 -0300

    added some Area and Posts tests

--- AlmappCoreTests/API/ALMPostsTests.m
+++ AlmappCoreTests/API/ALMPostsTests.m
@@ -21,14 +21,46 @@
     [self resource:[ALMPost class] ID:1 path:nil params:nil afterSuccess:^(id result) {
         ALMPost *post = result;
         
-        XCTAssertNotNil(post, @"Must rerturn a valid parent object.");
-        XCTAssertNotNil(post.user, @"Must rerturn a valid parent object.");
-        XCTAssertNotNil(post.target, @"Must rerturn a valid parent object.");
+        XCTAssertNotNil(post, @"Must rerturn a valid object.");
+        XCTAssertNotNil(post.user, @"Must rerturn a valid nested object.");
+        XCTAssertNotNil(post.target, @"Must rerturn a valid nested object.");
         
         XCTAssertEqualObjects(post.target.singleForm, [ALMCampus singleForm]);
     }];
     
+    [self resource:[ALMPost class] ID:3 path:nil params:nil afterSuccess:^(id result) {
+        ALMPost *post = result;
+        
+        XCTAssertNotNil(post, @"Must rerturn a valid object.");
+        XCTAssertNotNil(post.user, @"Must rerturn a valid nested object.");
+        XCTAssertNotNil(post.target, @"Must rerturn a valid nested object.");
+        
+        XCTAssertEqualObjects(post.target.singleForm, [ALMAcademicUnity singleForm]);
+    }];
     
+    [self resource:[ALMPost class] ID:4 path:nil params:nil afterSuccess:^(id result) {
+        ALMPost *post = result;
+        
+        XCTAssertNotNil(post, @"Must rerturn a valid object.");
+        XCTAssertNotNil(post.user, @"Must rerturn a valid nested object.");
+        XCTAssertNotNil(post.target, @"Must rerturn a valid nested object.");
+        XCTAssertNotNil(post.localization, @"Must rerturn a valid nested object.");
+        
+        XCTAssertEqualObjects(post.target.singleForm, [ALMCampus singleForm]);
+        XCTAssertEqualObjects(post.localization.identifier, @"ENF_219");
+    }];
+    
+    [self resource:[ALMPost class] ID:5 path:nil params:nil afterSuccess:^(id result) {
+        ALMPost *post = result;
+        
+        XCTAssertNotNil(post, @"Must rerturn a valid object.");
+        XCTAssertNotNil(post.user, @"Must rerturn a valid nested object.");
+        XCTAssertNotNil(post.target, @"Must rerturn a valid nested object.");
+        XCTAssertNotNil(post.event, @"Must rerturn a valid nested object.");
+        
+        XCTAssertEqualObjects(post.target.singleForm, [ALMBuilding singleForm]);
+        XCTAssertEqualObjects(post.event.title, @"Example Event");
+    }];
 }
 
 @end
