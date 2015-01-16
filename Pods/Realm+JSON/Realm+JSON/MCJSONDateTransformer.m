057068bc266546559956f9d2263c56d0bbb84a79
Patricio López <lopezjuripatricio@gmail.com>
2015-01-16 16:23:17 -0300

    updated custom Realm+JSON to match date format

--- Pods/Realm+JSON/Realm+JSON/MCJSONDateTransformer.m
+++ Pods/Realm+JSON/Realm+JSON/MCJSONDateTransformer.m
@@ -12,7 +12,7 @@ NSString* const MCJSONDateTimeTransformerName = @"MCJSONDateTimeTransformerName"
 NSString* const MCJSONDateTimeMillisecondTransformerName = @"MCJSONDateTimeMillisecondTransformerName";
 NSString* const MCJSONDateOnlyTransformerName = @"MCJSONDateOnlyTransformerName";
 static NSString *const kDateFormatDateTime = @"yyyy-MM-dd'T'HH:mm:ssZZZZZ";
-static NSString *const kDateFormatDateTimeMillisecond = @"yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ";
+static NSString *const kDateFormatDateTimeMillisecond = @"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
 static NSString *const kDateFormatDateOnly = @"yyyy-MM-dd";
 
 @interface MCJSONDateTransformer ()
