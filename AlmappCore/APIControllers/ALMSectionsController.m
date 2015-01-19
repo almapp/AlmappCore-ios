214685ee72e8e5e82c54404ed648b88ada3eb3b9
Patricio López <lopezjuripatricio@gmail.com>
2015-01-19 16:45:58 -0300

    added year and period to core

---
+++ AlmappCore/APIControllers/ALMSectionsController.m
@@ -0,0 +1,13 @@
+//
+//  ALMSectionsController.m
+//  AlmappCore
+//
+//  Created by Patricio López on 19-01-15.
+//  Copyright (c) 2015 almapp. All rights reserved.
+//
+
+#import "ALMSectionsController.h"
+
+@implementation ALMSectionsController
+
+@end
