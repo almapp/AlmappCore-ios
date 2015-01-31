7b4ba9b52f59e637401cb699e2fa1ac33b198e0e
Patricio López <lopezjuripatricio@gmail.com>
2015-01-31 19:40:28 -0300

    cleaned session manager

--- AlmappCore/Session/ALMSessionManager.m
+++ AlmappCore/Session/ALMSessionManager.m
@@ -20,36 +20,24 @@
     return [[super alloc] initWithCoreModuleDelegate:coreDelegate];
 }
 
-- (void)successfullyLoggedWithEmail:(NSString *)email didSave:(BOOL)saved {
-    RLMRealm *realm = nil;
-    if (_sessionManagerDelegate && [_sessionManagerDelegate respondsToSelector:@selector(sessionManagerSessionsStoreRealm:)]) {
-        realm = [_sessionManagerDelegate sessionManagerSessionsStoreRealm:self];
-    }
-    else {
-        realm = [RLMRealm defaultRealm];
-    }
-    
-    ALMSession *newSession = [ALMSession objectInRealm:realm forPrimaryKey:email];
-    
-    if ([_sessionManagerDelegate respondsToSelector:@selector(sessionManager:successfullyLoggedAs:)]) {
-        //[_sessionManagerDelegate sessionManager:self successfullyLoggedAs:<#(ALMSession *)#>];
-    }
-}
-
 - (void)setCurrentSession:(ALMSession *)newSession {
-    if ([_sessionManagerDelegate respondsToSelector:@selector(sessionManager:shouldChangeToSession:)]) {
+    if (_sessionManagerDelegate && [_sessionManagerDelegate respondsToSelector:@selector(sessionManager:shouldChangeToSession:)]) {
         if (![_sessionManagerDelegate sessionManager:self shouldChangeToSession:newSession]) {
             return;
         }
     }
     _actualSession = newSession;
-     if ([_sessionManagerDelegate respondsToSelector:@selector(sessionManager:didChangeSessionTo:)]) {
+     if (_sessionManagerDelegate && [_sessionManagerDelegate respondsToSelector:@selector(sessionManager:didChangeSessionTo:)]) {
          [_sessionManagerDelegate sessionManager:self didChangeSessionTo:newSession];
      }
 }
 
 - (ALMSession *)currentSession {
-    return _actualSession;
+    if (_sessionManagerDelegate && [_sessionManagerDelegate respondsToSelector:@selector(sessionManagerGetCurrentSession)]) {
+        return [_sessionManagerDelegate sessionManagerGetCurrentSession];
+    } else {
+        return _actualSession;
+    }
 }
 
 - (RLMResults *)availableSessions {
